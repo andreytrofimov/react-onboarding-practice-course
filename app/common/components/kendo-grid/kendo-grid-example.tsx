b0a772758126e869e1b6a12a2d192655bfed498a
Andrey Trofimov <aegaeon51@gmail.com>
2019-06-01 00:02:31 +0300

    Created PasswordCell component

--- app/common/components/kendo-grid/kendo-grid-example.tsx
+++ app/common/components/kendo-grid/kendo-grid-example.tsx
@@ -5,6 +5,7 @@ import { observable, action } from 'mobx';
 
 import { Stack, Button } from '@servicetitan/design-system';
 import { GridColumn } from '@progress/kendo-react-grid';
+import { getPasswordCell } from './editable-cell/get-password-cell';
 
 import { KendoGridState, InMemoryDataSource } from './kendo-grid-state';
 
@@ -33,6 +34,7 @@ interface Product {
     UnitsInStock: number;
     UnitsOnOrder: Date;
     Discontinued: boolean;
+    Password?: string;
     AvailableFor?: UserRole;
 }
 
@@ -205,6 +207,7 @@ export class GridExample extends React.Component {
                     <GridColumn field="UnitsOnOrder" title="First Ordered On" filter="date" editor="date" format="{0:d}" columnMenu={DateRangeColumnMenuFilter} />
                     <GridColumn field="UnitPrice" title="Unit Price" filter="numeric" editor="numeric" format="{0:c}" columnMenu={CurrencyRangeColumnMenuFilter} />
                     <GridColumn field="AvailableFor" title="Available For" cell={getSelectCell(getEnumValues(UserRole))} />
+                    <GridColumn field="Password" title="Password" cell={getPasswordCell()} />
                 </KendoGrid>
             </React.Fragment>
         );
