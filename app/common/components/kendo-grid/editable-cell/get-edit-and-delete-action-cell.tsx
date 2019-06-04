1178804dc6ada999d2b90b4415875548fc3e1653
Andrey Trofimov <aegaeon51@gmail.com>
2019-06-04 23:39:11 +0300

    Created the Delete Confirmation component

--- app/common/components/kendo-grid/editable-cell/get-edit-and-delete-action-cell.tsx
+++ app/common/components/kendo-grid/editable-cell/get-edit-and-delete-action-cell.tsx
@@ -1,13 +1,16 @@
 import { GridCell, GridCellProps } from '@progress/kendo-react-grid';
 import * as React from 'react';
 
-import { Button } from '@servicetitan/design-system';
+import { Button, Dialog } from '@servicetitan/design-system';
+
+import { Confirm, ConfirmationProps } from '../../confirm/confirm';
 
 interface GetEditAndDeleteActionCellParams<T> {
     onEdit?: (dataItem: T) => void;
     onSave?: (dataItem: T) => void;
     onCancel?: (dataItem: T) => void;
     onDelete?: (dataItem: T) => void;
+    deleteConfirmation?: boolean;
 }
 
 export function getEditAndDeleteActionCell<T>({
@@ -15,6 +18,7 @@ export function getEditAndDeleteActionCell<T>({
     onSave,
     onCancel,
     onDelete,
+    deleteConfirmation = true,
 }: GetEditAndDeleteActionCellParams<T>): React.ComponentType<GridCellProps> {
     return class extends GridCell {
         render() {
@@ -43,10 +47,50 @@ export function getEditAndDeleteActionCell<T>({
                         <Button small text primary onClick={() => onEdit(dataItem)}>Edit</Button>
                     )}
                     {onDelete && (
-                        <Button small text negative onClick={() => onDelete(dataItem)}>Delete</Button>
+                        <DeleteButton
+                            onDelete={() => onDelete(dataItem)}
+                            deleteConfirmation={deleteConfirmation}
+                        />
                     )}
                 </td>
             );
         }
     };
 }
+
+interface DeleteButtonProps {
+    onDelete: () => void;
+    deleteConfirmation?: boolean;
+}
+
+const DeleteButton: React.FC<DeleteButtonProps> = ({
+    onDelete,
+    deleteConfirmation,
+}) =>
+    (
+        deleteConfirmation
+            ? (
+                <Confirm onClick={onDelete} confirmation={DeleteConfirmation}>
+                    {onClick => (
+                        <Button small text negative onClick={onClick}>Delete</Button>
+                    )}
+                </Confirm>
+            )
+            : (
+                <Button small text negative onClick={onDelete}>Delete</Button>
+            )
+    );
+
+
+const DeleteConfirmation: React.FC<ConfirmationProps> = ({ onConfirm, onCancel }) => (
+    <Dialog
+        open
+        onClose={onCancel}
+        title="Delete User"
+        negative
+        onPrimaryActionClick={onConfirm}
+        primaryActionName="Delete"
+        onSecondaryActionClick={onCancel}
+        secondaryActionName="Cancel"
+    >Are you sure you want to delete this user?</Dialog>
+);
