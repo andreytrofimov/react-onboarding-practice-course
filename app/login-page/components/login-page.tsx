8a5af8ee61dfcc1e6ce184c6f0d1772d60c18e50
Andrey Trofimov <aegaeon51@gmail.com>
2019-06-06 00:01:45 +0300

    LoginPage: Fixes from review

--- app/login-page/components/login-page.tsx
+++ app/login-page/components/login-page.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { Button, Form, Link, Stack, Text } from '@servicetitan/design-system';
+import { Button, ButtonGroup, Form, Link, Stack, Text } from '@servicetitan/design-system';
 
 import * as Styles from './login-page.less'
 
@@ -11,14 +11,14 @@ export const LoginPage = () => {
         <Stack alignItems="center" justifyContent="center" className="flex-auto">
             <Stack.Item className={Styles.formContainer}>
                 <Form onSubmit={save}>
-                    <Text size={5} bold className={Styles.header}>Login</Text>
+                    <Text size={5} bold className="ta-center">Login</Text>
                     <Form.Input label="Login" />
                     <Form.Input label="Password" type="password" />
 
-                    <Stack alignItems="center" justifyContent="space-between">
+                    <ButtonGroup fullWidth>
                         <Link href="/#/registration" color="primary">Sign up</Link>
-                        <Button className={Styles.loginButton} primary type="submit">Login</Button>
-                    </Stack>
+                        <Button className="w-50" primary type="submit">Login</Button>
+                    </ButtonGroup>
                 </Form>
             </Stack.Item>
         </Stack>
