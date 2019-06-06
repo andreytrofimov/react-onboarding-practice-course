29b737d10ac16b0c143035b99927ab2b17a1039d
Andrey Trofimov <aegaeon51@gmail.com>
2019-06-06 17:05:40 +0300

    UsersPage: refactored to FC

--- app/users-page/components/users-grid.tsx
+++ app/users-page/components/users-grid.tsx
@@ -1,7 +1,8 @@
 import { GridColumn } from '@progress/kendo-react-grid';
 
-import { observer } from 'mobx-react';
+import { observer } from 'mobx-react-lite';
 import * as React from 'react';
+import { useEffect, useMemo } from 'react';
 
 import { getEditAndDeleteActionCell } from '../../common/components/kendo-grid/editable-cell/get-edit-and-delete-action-cell';
 import { getSelectCell } from '../../common/components/kendo-grid/editable-cell/get-select-cell';
@@ -12,43 +13,54 @@ import { getEnumValues } from '../../common/utils/form-helpers';
 import { Users } from '../../mocks/user';
 import { Role, User } from '../../user/models';
 
-@observer
-export class UsersGrid extends React.Component {
-    private gridState = new KendoGridState<User, User['id']>({
-        pageSize: 5,
-        idSelector: u => u.id,
-    });
+type UsersGridState = KendoGridState<User, User['id']>;
 
-    componentDidMount() {
-        this.gridState.setDataSource(new InMemoryDataSource(Users));
+export const UsersGrid = observer(() => {
+    const gridState = useUsersGridState();
+
+    const ActionCell = useActionCellMemo(gridState);
+
+    return (
+        <KendoGrid
+            gridState={gridState}
+            selectable
+            sortable
+            groupable={{ footer: 'visible' }}
+        >
+            <GridColumn field="id" title="id" filterable={false} editable={false} width="100px" />
+            <GridColumn field="login" title="login" width="240px" />
+            <GridColumn field="password" title="password" cell={PasswordCell} />
+            <GridColumn field="role" title="role" cell={SelectRoleCell} />
+            <GridColumn title="Actions" cell={ActionCell} />
+        </KendoGrid>
+    );
+});
+
+function useUsersGridState(): UsersGridState {
+    function factory() {
+        return new KendoGridState<User, User['id']>({
+            pageSize: 5,
+            idSelector: u => u.id,
+        });
     }
 
-    SelectRoleCell = getSelectCell(getEnumValues(Role));
-    ActionCell = createActionCell(this.gridState);
+    const gridState = useMemo(factory, []);
 
-    render() {
-        return (
-            <KendoGrid
-                gridState={this.gridState}
-                selectable
-                sortable
-                groupable={{ footer: 'visible' }}
-            >
-                <GridColumn field="id" title="id" filterable={false} editable={false} width="100px" />
-                <GridColumn field="login" title="login" width="240px" />
-                <GridColumn field="password" title="password" cell={PasswordCell} />
-                <GridColumn field="role" title="role" cell={this.SelectRoleCell} />
-                <GridColumn title="Actions" cell={this.ActionCell} />
-            </KendoGrid>
-        );
-    }
+    useEffect(() => {
+        gridState.setDataSource(new InMemoryDataSource(Users));
+    }, []);
+
+    return gridState;
 }
 
-function createActionCell(gridState: KendoGridState<User, User['id']>) {
-    return getEditAndDeleteActionCell<User>({
+const SelectRoleCell = getSelectCell(getEnumValues(Role));
+
+function useActionCellMemo(gridState: UsersGridState) {
+    return useMemo(() => getEditAndDeleteActionCell<User>({
         onEdit: u => gridState.edit(u),
         onSave: u => gridState.saveEdit(u),
         onCancel: u => gridState.cancelEdit(u),
         onDelete: u => gridState.removeFromDataSource(i => i.id === u.id),
-    })
+    }), []);
 }
+
