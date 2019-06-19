9eaadfe11ca14c7ff2761d2bbee4b9577c7f7113
Andrey Trofimov <aegaeon51@gmail.com>
2019-06-19 07:15:21 +0300

    Created mock API service

--- app/news-page/api/news.api.ts
+++ app/news-page/api/news.api.ts
@@ -1,12 +1,75 @@
 import { AxiosPromise } from 'axios';
 
+import { injectable } from '@servicetitan/react-ioc';
+
+import { mockResponse } from '../../mocks/api';
+import { NewsDB } from '../../mocks/post.db';
 import { Post } from '../models/post';
 
 export interface INewsApi {
     getAll(): AxiosPromise<Post[]>;
     create(data: Post): AxiosPromise<Post | undefined>;
     edit(data: Post): AxiosPromise<Post | undefined>;
-    delete(data: DeletePostDTO): AxiosPromise<void>;
+    delete(id: DeletePostDTO): AxiosPromise<void>;
 }
 
 export type DeletePostDTO = Required<Post>['id'];
+
+@injectable()
+export class NewsApi implements INewsApi {
+    getAll() {
+        return mockResponse({
+            data: void 0,
+            response: NewsDB.get(),
+            log: 'NewsApi.getAll'
+        });
+    }
+
+    create(data: Post) {
+        NewsDB.create(data);
+
+        return mockResponse({
+            log: 'NewsApi.register',
+            data: void 0,
+            response: void 0,
+        });
+    }
+
+    edit(data: Post) {
+        const post = NewsDB.update(data);
+        if (!post) {
+            return mockResponse({
+                status: 404,
+                statusText: 'Cannot find post',
+                log: 'NewsApi.edit',
+                data,
+                response: post,
+            });
+        }
+
+        return mockResponse({
+            log: 'NewsApi.edit',
+            data,
+            response: post,
+        });
+    }
+
+    delete(id: DeletePostDTO) {
+        const success = NewsDB.delete(id);
+        if (!success) {
+            return mockResponse({
+                status: 404,
+                statusText: 'Cannot find post',
+                log: 'NewsApi.delete',
+                data: id,
+                response: void 0,
+            });
+        }
+
+        return mockResponse({
+            log: 'NewsApi.delete',
+            data: id,
+            response: void 0,
+        });
+    }
+}
